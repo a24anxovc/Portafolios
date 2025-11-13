8d7d95ebadce414701bbaeaa8cda3f1958db4bc3
a24anxovc <a24anxovc@iessanclemente.net>
2025-11-13 21:14:09 +0100

    Delete js directory

--- js/responsiveMenu.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const menu = document.getElementById('menu')
-    const navMenu = document.querySelector('.nav--menu')
-    menu.addEventListener('click', function(){
-        navMenu.classList.toggle('transalate--menu')
-    })
-})
